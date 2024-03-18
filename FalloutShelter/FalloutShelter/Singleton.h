e0f82975d5589dee71acc69dbdda9c00e20df8c6
Tatou12 <t.martin02@free.fr>
2024-03-18 17:02:49 +0100

    blbl

---
+++ FalloutShelter/FalloutShelter/Singleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template <typename Class>
+class Singleton
+{
+protected:
+	static Class instance;
+
+public:
+	static Class& GetInstance()
+	{
+		return instance;
+	}
+};
+
+template <typename Class>
+Class Singleton<Class>::instance;
+
---
+++ FalloutShelter/FalloutShelter/Widget.cpp
@@ -0,0 +1,7 @@
+#include "Widget.h"
+Widget::Widget(const WidgetType& _type)
+{
+	isVisible = true;
+	applyShader = false;
+	type = _type;
+}
